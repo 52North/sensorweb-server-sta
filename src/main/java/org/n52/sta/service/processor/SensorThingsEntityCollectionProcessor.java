d689301e3d582307d8591dfd576afb45838129da
SebaDro <sebastian.drost@hs-bochum.de>
2018-08-20 14:45:58 +0200

    Adjust entity annotation strategy in order to set base URL for navigation links

--- src/main/java/org/n52/sta/service/processor/SensorThingsEntityCollectionProcessor.java
+++ src/main/java/org/n52/sta/service/processor/SensorThingsEntityCollectionProcessor.java
@@ -42,7 +42,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SensorThingsEntityCollectionProcessor implements EntityCollectionProcessor {
 
-	private static final ContentType ET_COLLECTION_PROCESSOR_CONTENT_TYPE = ContentType.JSON_FULL_METADATA;
+	private static final ContentType ET_COLLECTION_PROCESSOR_CONTENT_TYPE = ContentType.JSON_NO_METADATA;
 
 	@Autowired
 	ThingService thingService;
@@ -69,7 +69,7 @@ public class SensorThingsEntityCollectionProcessor implements EntityCollectionPr
 		
 		// 2nd: fetch the data from backend for this requested EntitySetName and deliver as EntitySet
 		EntityCollection entityCollection = this.getEntityCollection(uriInfo.getUriResourceParts().get(0).toString());
-		entityCollection.forEach(e -> entityAnnotator.annotateEntity(e, edmEntitySet.getEntityType()));
+		entityCollection.forEach(e -> entityAnnotator.annotateEntity(e, edmEntitySet.getEntityType(), request.getRawBaseUri()));
 		
 		// 3rd: create a serializer based on the requested format (json)
 		ODataSerializer serializer = odata.createSerializer(ET_COLLECTION_PROCESSOR_CONTENT_TYPE);
--- src/main/java/org/n52/sta/utils/EntityAnnotator.java
+++ src/main/java/org/n52/sta/utils/EntityAnnotator.java
@@ -14,6 +14,8 @@ import org.apache.olingo.commons.api.edm.EdmNavigationProperty;
 import static org.n52.sta.edm.provider.entities.AbstractSensorThingsEntityProvider.ID_ANNOTATION;
 import static org.n52.sta.edm.provider.entities.AbstractSensorThingsEntityProvider.NAVIGATION_LINK_ANNOTATION;
 import static org.n52.sta.edm.provider.entities.AbstractSensorThingsEntityProvider.SELF_LINK_ANNOTATION;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,14 +25,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class EntityAnnotator {
 
-    public Entity annotateEntity(Entity entity, EdmEntityType entityType) {
-        entity.addProperty(new Property(null, SELF_LINK_ANNOTATION, ValueType.PRIMITIVE, entity.getId().getPath()));
+    public Entity annotateEntity(Entity entity, EdmEntityType entityType, String baseUri) {
+        String selfLink = String.join("/", baseUri, entity.getId().getPath());
+        entity.addProperty(new Property(null, SELF_LINK_ANNOTATION, ValueType.PRIMITIVE, selfLink));
 
         entityType.getNavigationPropertyNames().forEach(np -> {
             String navigationAnnotationName = np + NAVIGATION_LINK_ANNOTATION;
             EdmNavigationProperty navProp = entityType.getNavigationProperty(np);
 
-            String navigationAnnotationValue = String.join("/", entity.getId().getPath(), navProp.getName());
+            String navigationAnnotationValue = String.join("/", baseUri, entity.getId().getPath(), navProp.getName());
             entity.addProperty(new Property(null, navigationAnnotationName, ValueType.PRIMITIVE, navigationAnnotationValue));
         });
 
