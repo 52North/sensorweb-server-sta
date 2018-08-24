2ec83107f20e9b5ab538406f849718fb5ac12628
SebaDro <sebastian.drost@hs-bochum.de>
2018-08-24 09:59:47 +0200

    Prepare abstract entity service for entity fetching

--- src/main/java/org/n52/sta/data/AbstractSensorThingsEntityService.java
+++ src/main/java/org/n52/sta/data/AbstractSensorThingsEntityService.java
@@ -5,9 +5,12 @@
  */
 package org.n52.sta.data;
 
+import java.util.List;
 import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.data.EntityCollection;
+import org.apache.olingo.server.api.uri.UriParameter;
 import org.n52.series.db.beans.sta.AbstractStaEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,9 +18,16 @@ import org.springframework.stereotype.Component;
  * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
  */
 public interface AbstractSensorThingsEntityService {
-
+    
     public EntityCollection getEntityCollection();
 
-    public Entity getEntityForId(String id);
+    public EntityCollection getRelatedEntityCollection(Entity sourceEntity);
+
+    public Entity getEntity(List<UriParameter> keyPredicates);
+
+    public Entity getRelatedEntity(Entity sourceEntity);
+
+    public Entity getRelatedEntity(Entity sourceEntity, List<UriParameter> keyPredicates);
+
 
 }
