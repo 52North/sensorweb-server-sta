f965bb8cc2ccbbd0e681ad960c41e9489d248868
SebaDro <sebastian.drost@hs-bochum.de>
2018-08-24 16:14:13 +0200

    Add handler for entity collection requests

---
+++ src/main/java/org/n52/sta/service/handler/AbstractEntityCollectionRequestHandler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.sta.service.handler;
+
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.uri.UriInfo;
+import org.n52.sta.service.response.EntityCollectionResponse;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public abstract class AbstractEntityCollectionRequestHandler {
+
+    public abstract EntityCollectionResponse handleEntityCollectionRequest(UriInfo uriInfo) throws ODataApplicationException;
+
+}
---
+++ src/main/java/org/n52/sta/service/response/EntityCollectionResponse.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.sta.service.response;
+
+import org.apache.olingo.commons.api.data.EntityCollection;
+import org.apache.olingo.commons.api.edm.EdmEntitySet;
+
+/**
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class EntityCollectionResponse {
+
+    private EntityCollection entityCollection;
+
+    private EdmEntitySet entitySet;
+
+    public EntityCollection getEntityCollection() {
+        return entityCollection;
+    }
+
+    public void setEntityCollection(EntityCollection entityCollection) {
+        this.entityCollection = entityCollection;
+    }
+
+    public EdmEntitySet getEntitySet() {
+        return entitySet;
+    }
+
+    public void setEntitySet(EdmEntitySet entitySet) {
+        this.entitySet = entitySet;
+    }
+
+}
