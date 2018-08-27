19be6b4c0548bf7d135983a748d1670dcaaa0e0f
SebaDro <sebastian.drost@hs-bochum.de>
2018-08-27 16:27:50 +0200

    Introduce handler and processor for property requests

--- src/main/java/org/n52/sta/service/SensorThingsController.java
+++ src/main/java/org/n52/sta/service/SensorThingsController.java
@@ -17,6 +17,7 @@ import org.apache.olingo.server.api.ServiceMetadata;
 import org.apache.olingo.server.api.processor.EntityCollectionProcessor;
 import org.apache.olingo.server.api.processor.EntityProcessor;
 import org.apache.olingo.server.api.processor.ErrorProcessor;
+import org.apache.olingo.server.api.processor.PrimitiveProcessor;
 import org.apache.olingo.server.api.processor.ServiceDocumentProcessor;
 import static org.n52.sta.service.SensorThingsController.URI;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,45 +31,49 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/" + URI)
 public class SensorThingsController {
-    
+
     public static final String URI = "sta";
-    
+
     @Autowired
     private CsdlAbstractEdmProvider provider;
-    
+
     @Autowired
     private ServiceDocumentProcessor serviceDocumentProcessor;
-    
+
     @Autowired
     private EntityCollectionProcessor entityCollectionProcessor;
-    
+
     @Autowired
     private EntityProcessor entityProcessor;
-    
+
+    @Autowired
+    private PrimitiveProcessor primitiveProcessor;
+
     @Autowired
     private ErrorProcessor errorProcessor;
-    
+
     @RequestMapping("**")
     protected void process(HttpServletRequest request, HttpServletResponse response) {
 
         // create odata handler and configure it with EdmProvider and Processor
         OData odata = OData.newInstance();
         ServiceMetadata edm = odata.createServiceMetadata(provider, new ArrayList<EdmxReference>());
-        
+
         ODataHttpHandler handler = odata.createHandler(edm);
         handler.register(serviceDocumentProcessor);
         handler.register(entityCollectionProcessor);
         handler.register(entityProcessor);
+        handler.register(primitiveProcessor);
         handler.register(errorProcessor);
 
         // let the handler do the work
         handler.process(new HttpServletRequestWrapper(request) {
-            
+
             @Override
             public String getServletPath() {
                 return URI;
             }
         }, response);
-        
+
     }
 }
---
+++ src/main/java/org/n52/sta/service/handler/PropertyRequestHandlerImpl.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.n52.sta.service.handler;
+
+import java.util.List;
+import java.util.Locale;
+import org.apache.olingo.commons.api.data.Entity;
+import org.apache.olingo.commons.api.data.Property;
+import org.apache.olingo.commons.api.edm.EdmEntitySet;
+import org.apache.olingo.commons.api.edm.EdmProperty;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.apache.olingo.server.api.uri.UriInfo;
+import org.apache.olingo.server.api.uri.UriParameter;
+import org.apache.olingo.server.api.uri.UriResource;
+import org.apache.olingo.server.api.uri.UriResourceEntitySet;
+import org.apache.olingo.server.api.uri.UriResourceNavigation;
+import org.apache.olingo.server.api.uri.UriResourceProperty;
+import org.n52.sta.data.AbstractSensorThingsEntityService;
+import org.n52.sta.data.EntityServiceRepository;
+import org.n52.sta.service.response.PropertyResponse;
+import org.n52.sta.utils.NavigationLink;
+import org.n52.sta.utils.UriResourceNavigationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ ** Implementation for handling property requests
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Component
+public class PropertyRequestHandlerImpl implements AbstractPropertyRequestHandler {
+
+    @Autowired
+    private EntityServiceRepository serviceRepository;
+
+    @Autowired
+    private UriResourceNavigationResolver navigationResolver;
+
+    @Override
+    public PropertyResponse handlePrimitiveRequest(UriInfo uriInfo) throws ODataApplicationException {
+        PropertyResponse response = null;
+
+        List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
+
+        // handle request depending on the number of UriResource paths
+        // e.g the case: sta/Things(id)/description
+        if (resourcePaths.size() == 2) {
+            response = resolvePropertyForEntity(resourcePaths);
+
+            // e.g. the case: sta/Things(id)/Locations(id)/name
+        } else {
+            response = resolvePropertyForNavigation(resourcePaths);
+
+        }
+        return response;
+    }
+
+    private PropertyResponse resolvePropertyForEntity(List<UriResource> resourcePaths) throws ODataApplicationException {
+        // determine the response EntitySet
+        UriResourceEntitySet uriResourceEntitySet = navigationResolver.resolveRootUriResource(resourcePaths.get(0));
+        EdmEntitySet responseEntitySet = uriResourceEntitySet.getEntitySet();
+
+        // fetch the data from backend for this requested Entity and deliver as Entity
+        List<UriParameter> keyPredicates = uriResourceEntitySet.getKeyPredicates();
+        AbstractSensorThingsEntityService responseService = serviceRepository.getEntityService(uriResourceEntitySet.getEntityType().getName());
+        Entity targetEntity = responseService.getEntity(keyPredicates);
+
+        if (targetEntity == null) {
+            throw new ODataApplicationException("Entity not found.",
+                    HttpStatusCode.NOT_FOUND.getStatusCode(), Locale.ROOT);
+        }
+        UriResourceProperty uriProperty = (UriResourceProperty) resourcePaths.get(resourcePaths.size() - 1);
+
+        PropertyResponse response = resolveProperty(targetEntity, uriProperty, responseEntitySet);
+
+        return response;
+    }
+
+    private PropertyResponse resolvePropertyForNavigation(List<UriResource> resourcePaths) throws ODataApplicationException {
+        // determine the last NavigationLink and fetch Entity for it
+        NavigationLink lastNavigationLink = navigationResolver.resolveUriResourceNavigationPaths(resourcePaths.subList(0, resourcePaths.size()));
+
+        UriResource lastEntitySegment = resourcePaths.get(resourcePaths.size() - 2);
+        Entity targetEntity = null;
+        PropertyResponse response = null;
+        if (lastEntitySegment instanceof UriResourceNavigation) {
+
+            List<UriParameter> navKeyPredicates = ((UriResourceNavigation) lastEntitySegment).getKeyPredicates();
+
+            // e.g. /HistoricalLocations(id)/Thing/description
+            if (navKeyPredicates.isEmpty()) {
+                targetEntity = serviceRepository.getEntityService(lastNavigationLink.getTargetEntitySet().getEntityType().getName())
+                        .getRelatedEntity(lastNavigationLink.getSourceEntity());
+
+            } else { // e.g. /Things(id)/Locations(id)/description
+                targetEntity = serviceRepository.getEntityService(lastNavigationLink.getTargetEntitySet().getEntityType().getName())
+                        .getRelatedEntity(lastNavigationLink.getSourceEntity(), navKeyPredicates);
+            }
+            if (targetEntity == null) {
+                throw new ODataApplicationException("Entity not found.",
+                        HttpStatusCode.NOT_FOUND.getStatusCode(), Locale.ROOT);
+            }
+
+            UriResourceProperty uriProperty = (UriResourceProperty) resourcePaths.get(resourcePaths.size() - 1);
+
+            response = resolveProperty(targetEntity, uriProperty, lastNavigationLink.getTargetEntitySet());
+
+        }
+        return response;
+    }
+
+    private PropertyResponse resolveProperty(Entity targetEntity, UriResourceProperty uriProperty, EdmEntitySet targetEntitySet) throws ODataApplicationException {
+        EdmProperty edmProperty = uriProperty.getProperty();
+        String edmPropertyName = edmProperty.getName();
+
+        // retrieve the property data from the entity
+        Property property = targetEntity.getProperty(edmPropertyName);
+        if (property == null) {
+            throw new ODataApplicationException("Property not found",
+                    HttpStatusCode.NOT_FOUND.getStatusCode(), Locale.ENGLISH);
+        }
+
+        // set Entity response information
+        PropertyResponse response = new PropertyResponse();
+        response.setEdmPropertyType(edmProperty.getType());
+        response.setProperty(property);
+        response.setResponseEdmEntitySet(targetEntitySet);
+
+        return response;
+    }
+
+}
