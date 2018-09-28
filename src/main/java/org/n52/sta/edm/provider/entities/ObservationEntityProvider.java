578c5eb47f6a196cb4adbe863e16be3a1fc6f8cc
Carsten Hollmann <c.hollmann@52north.org>
2018-09-28 12:49:37 +0200

    add precision for result time to fix IAE

--- src/main/java/org/n52/sta/edm/provider/entities/ObservationEntityProvider.java
+++ src/main/java/org/n52/sta/edm/provider/entities/ObservationEntityProvider.java
@@ -84,7 +84,7 @@ public class ObservationEntityProvider extends AbstractSensorThingsEntityProvide
                 .setNullable(false);
         CsdlProperty resultTime = new CsdlProperty().setName(PROP_RESULT_TIME)
                 .setType(EdmPrimitiveTypeKind.DateTimeOffset.getFullQualifiedName())
-                .setNullable(false);
+                .setNullable(false).setPrecision(9);
         CsdlProperty resultQuality = new CsdlProperty().setName(PROP_RESULT_QUALITY)
                 .setType(EdmPrimitiveTypeKind.String.getFullQualifiedName())
                 .setNullable(true);
