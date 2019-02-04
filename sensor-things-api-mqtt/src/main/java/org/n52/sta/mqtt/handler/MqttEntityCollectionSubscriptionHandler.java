df9e3eb7052e9980120006a921cb4f7e630086a0
SebaDro <sebastian.drost@hs-bochum.de>
2019-02-04 17:57:01 +0100

    Implement creation of MqttEntityCollectionSubscriptions

--- sensor-things-api-mqtt/src/main/java/org/n52/sta/mqtt/handler/MqttEntityCollectionSubscriptionHandler.java
+++ sensor-things-api-mqtt/src/main/java/org/n52/sta/mqtt/handler/MqttEntityCollectionSubscriptionHandler.java
@@ -5,73 +5,56 @@
  */
 package org.n52.sta.mqtt.handler;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import org.apache.olingo.commons.api.edm.EdmEntitySet;
 import org.apache.olingo.server.api.ODataApplicationException;
-import org.apache.olingo.server.api.uri.UriInfo;
 import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
-import org.apache.olingo.server.api.uri.queryoption.SelectItem;
-import org.n52.sta.mqtt.core.MqttSubscription;
-import org.n52.sta.service.handler.AbstractEntityCollectionRequestHandler;
-import org.n52.sta.service.handler.AbstractEntityRequestHandler;
+import org.n52.sta.mqtt.core.MqttEntityCollectionSubscription;
 import org.n52.sta.service.query.QueryOptions;
-import org.n52.sta.service.query.URIQueryOptions;
-import org.n52.sta.service.response.EntityCollectionResponse;
-import org.n52.sta.service.response.EntityResponse;
 import org.n52.sta.utils.EntityQueryParams;
 import org.n52.sta.utils.UriResourceNavigationResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
  */
+@Component
 public class MqttEntityCollectionSubscriptionHandler {
 
-//    @Autowired
-//    private UriResourceNavigationResolver navigationResolver;
-//
-//    public MQTTSubscription handleEntityCollectionRequest(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
-//        MQTTSubscription response = null;
-//
-//        // handle request depending on the number of UriResource paths
-//        // e.g the case: sta/Things
-//        if (resourcePaths.size() == 1) {
-//            response = createResponseForEntitySet(topic, resourcePaths, queryOptions);
-//
-//            // e.g. the case: sta/Things(id)/Locations
-//        } else {
-//            response = createResponseForNavigation(resourcePaths, queryOptions);
-//        }
-//        return response;
-//    }
-//
-//    private MQTTSubscription createResponseForEntitySet(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
-//        List<SelectItem> fields = new ArrayList();
-//        Set<String> watchedProperties = new HashSet();
-//        String olingoEntityType = null;
-//        boolean isCollection = true;
-//        Long entityId = null;
-//
-//        if (queryOptions.hasSelectOption()) {
-//            fields = queryOptions.getSelectOption().getSelectItems();
-//        }
-//
-//        // determine the response EntitySet
-//        UriResourceEntitySet uriResourceEntitySet = navigationResolver.resolveRootUriResource(resourcePaths.get(0));
-//        EdmEntitySet responseEntitySet = uriResourceEntitySet.getEntitySet();
-//        MQTTSubscription subscription = new MQTTSubscription(topic, fields, olingoEntityType, watchedProperties, isCollection, entityId, queryOptions, responseEntitySet, responseEntitySet.getEntityType());
-//        return subscription;
-//    }
-//
-//    private MQTTSubscription createResponseForNavigation(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
-//        // determine the target query parameters and fetch EntityCollection for it
-//        EntityQueryParams queryParams = navigationResolver.resolveUriResourceNavigationPaths(resourcePaths);
-//
-//    }
+    @Autowired
+    private UriResourceNavigationResolver navigationResolver;
+
+    public MqttEntityCollectionSubscription handleEntityCollectionRequest(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
+        MqttEntityCollectionSubscription response = null;
+
+        // handle request depending on the number of UriResource paths
+        // e.g the case: sta/Things
+        if (resourcePaths.size() == 1) {
+            response = createResponseForEntitySet(topic, resourcePaths, queryOptions);
+
+            // e.g. the case: sta/Things(id)/Locations
+        } else {
+            response = createResponseForNavigation(topic, resourcePaths, queryOptions);
+        }
+        return response;
+    }
+
+    private MqttEntityCollectionSubscription createResponseForEntitySet(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
+        // determine the response EntitySet
+        UriResourceEntitySet uriResourceEntitySet = navigationResolver.resolveRootUriResource(resourcePaths.get(0));
+        EdmEntitySet responseEntitySet = uriResourceEntitySet.getEntitySet();
+        MqttEntityCollectionSubscription subscription = new MqttEntityCollectionSubscription(topic, queryOptions, null, null, responseEntitySet, responseEntitySet.getEntityType());
+        return subscription;
+    }
+
+    private MqttEntityCollectionSubscription createResponseForNavigation(String topic, List<UriResource> resourcePaths, QueryOptions queryOptions) throws ODataApplicationException {
+        // determine the target query parameters
+        EntityQueryParams queryParams = navigationResolver.resolveUriResourceNavigationPaths(resourcePaths);
+        MqttEntityCollectionSubscription subscription = new MqttEntityCollectionSubscription(topic, queryOptions, queryParams.getSourceEntityType(), queryParams.getSourceId(), queryParams.getTargetEntitySet(), queryParams.getTargetEntitySet().getEntityType());
+        return subscription;
+    }
 
 }
