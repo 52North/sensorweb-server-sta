9fbd6236ff31cb8b7e85fbb12f957a16c90bfe20
Carsten Hollmann <c.hollmann@52north.org>
2019-04-29 14:53:53 +0200

    fix cycle

--- sensor-things-api-core/src/main/java/org/n52/sta/service/handler/crud/ObservationEntityCrudRequestHandler.java
+++ sensor-things-api-core/src/main/java/org/n52/sta/service/handler/crud/ObservationEntityCrudRequestHandler.java
@@ -32,7 +32,6 @@ import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.http.HttpMethod;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.n52.series.db.beans.DataEntity;
-import org.n52.sta.HibernateMessageInterceptor;
 import org.n52.sta.data.service.AbstractSensorThingsEntityService;
 import org.n52.sta.data.service.EntityServiceRepository.EntityTypes;
 import org.n52.sta.mapping.AbstractMapper;
@@ -45,9 +44,6 @@ public class ObservationEntityCrudRequestHandler extends AbstractEntityCrudReque
 
     @Autowired
     private ObservationMapper mapper;
-
-    @Autowired
-    private HibernateMessageInterceptor mqttInterceptor;
 
     @Override
     protected Entity handleCreateEntityRequest(Entity entity) throws ODataApplicationException {
