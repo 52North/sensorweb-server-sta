8d2a9681f65b8e0542aff7d2f8f417dd8a8ec4eb
speckij <j_spec05@uni-muenster.de>
2019-06-26 16:44:11 +0200

    Implemented missing entity property name translations to mqtt interceptor

--- sensor-things-api-core/src/main/java/org/n52/sta/data/repositories/MessageBusRepository.java
+++ sensor-things-api-core/src/main/java/org/n52/sta/data/repositories/MessageBusRepository.java
@@ -1,21 +1,21 @@
 package org.n52.sta.data.repositories;
 
-import org.n52.series.db.beans.PlatformEntity;
-import org.n52.series.db.beans.ProcedureEntity;
+import org.n52.series.db.beans.*;
+import org.n52.series.db.beans.sta.DatastreamEntity;
+import org.n52.series.db.beans.sta.HistoricalLocationEntity;
 import org.n52.series.db.beans.sta.LocationEntity;
 import org.n52.sta.SpringApplicationContext;
 import org.n52.sta.data.STAEventHandler;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
-import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class MessageBusRepository<T, ID extends Serializable>
         extends SimpleJpaRepository<T, ID> {
 
@@ -33,21 +33,18 @@ public class MessageBusRepository<T, ID extends Serializable>
         this.entityInformation = entityInformation;
 
         this.mqttHandler = (STAEventHandler) SpringApplicationContext.getBean("mqttEventHandler");
-    }
-
-    @PostConstruct
-    private void test() {
-        System.out.println("test");
+        Assert.notNull(this.mqttHandler, "Could not autowire Mqtt handler!");
     }
 
     @Transactional
     @Override
     public <S extends T> S save(S newEntity) {
         // Todo check if getname is the correct method
-        boolean intercept = mqttHandler.getWatchedEntityTypes().contains(entityInformation.getJavaType().getSimpleName());
+        boolean intercept = mqttHandler.getWatchedEntityTypes().contains(entityInformation.getJavaType().getName());
 
         if (entityInformation.isNew(newEntity)) {
             em.persist(newEntity);
+            em.flush();
             if (intercept) {
                 this.mqttHandler.handleEvent(newEntity, null);
             }
@@ -55,6 +52,7 @@ public class MessageBusRepository<T, ID extends Serializable>
             if (intercept) {
                 S oldEntity = (S) em.find(newEntity.getClass(), entityInformation.getId(newEntity));
                 newEntity = em.merge(newEntity);
+                em.flush();
                 this.mqttHandler.handleEvent(newEntity, computeDifferenceMap(oldEntity, newEntity));
             } else {
                 return em.merge(newEntity);
@@ -70,34 +68,78 @@ public class MessageBusRepository<T, ID extends Serializable>
             case "ProcedureEntity":
                 ProcedureEntity oldProcedure = (ProcedureEntity) oldE;
                 ProcedureEntity newProcedure = (ProcedureEntity) newE;
-                if (oldProcedure.getDescription().equals(newProcedure.getDescription())) map.add("description");
-                if (oldProcedure.getName().equals(newProcedure.getName())) map.add("name");
-                if (oldProcedure.getDescriptionFile().equals(newProcedure.getDescriptionFile())) map.add("metadata");
-                if (oldProcedure.getFormat().getFormat().equals(newProcedure.getFormat().getFormat())) map.add("encodingType");
+                if (!oldProcedure.getDescription().equals(newProcedure.getDescription())) map.add("description");
+                if (!oldProcedure.getName().equals(newProcedure.getName())) map.add("name");
+                if (!oldProcedure.getDescriptionFile().equals(newProcedure.getDescriptionFile())) map.add("metadata");
+                if (!oldProcedure.getFormat().getFormat().equals(newProcedure.getFormat().getFormat()))
+                    map.add("encodingType");
                 return map;
             case "LocationEntity":
                 LocationEntity oldLocation = (LocationEntity) oldE;
                 LocationEntity newLocation = (LocationEntity) newE;
-                if (oldLocation.getDescription().equals(newLocation.getDescription())) map.add("description");
-                if (oldLocation.getName().equals(newLocation.getName())) map.add("name");
-                if (oldLocation.getGeometryEntity().equals(newLocation.getGeometryEntity())) map.add("location");
-                if (oldLocation.getLocation().equals(newLocation.getLocation())) map.add("location");
-                if (oldLocation.getLocationEncoding().getEncodingType().equals(newLocation.getLocationEncoding().getEncodingType())) map.add("encodingType");
+                if (!oldLocation.getDescription().equals(newLocation.getDescription())) map.add("description");
+                if (!oldLocation.getName().equals(newLocation.getName())) map.add("name");
+                if (!oldLocation.getGeometryEntity().getGeometry().equals(newLocation.getGeometryEntity().getGeometry())) map.add("location");
+                if (!oldLocation.getLocation().equals(newLocation.getLocation())) map.add("location");
+                if (!oldLocation.getLocationEncoding().getEncodingType().equals(newLocation.getLocationEncoding().getEncodingType()))
+                    map.add("encodingType");
                 return map;
             case "PlatformEntity":
                 PlatformEntity oldThing = (PlatformEntity) oldE;
                 PlatformEntity newThing = (PlatformEntity) newE;
-                if (oldThing.getDescription().equals(newThing.getDescription())) map.add("description");
-                if (oldThing.getName().equals(newThing.getName())) map.add("name");
-                if (oldThing.getProperties().equals(newThing.getProperties())) map.add("properties");
+                if (!oldThing.getDescription().equals(newThing.getDescription())) map.add("description");
+                if (!oldThing.getName().equals(newThing.getName())) map.add("name");
+                if (!oldThing.getProperties().equals(newThing.getProperties())) map.add("properties");
                 return map;
-                //TODO: implement missing stuff
             case "DatastreamEntity":
+                DatastreamEntity oldDatastream = (DatastreamEntity) oldE;
+                DatastreamEntity newDatastream = (DatastreamEntity) newE;
+                if (!oldDatastream.getDescription().equals(newDatastream.getDescription())) map.add("description");
+                if (!oldDatastream.getName().equals(newDatastream.getName())) map.add("name");
+                if (!oldDatastream.getObservationType().getFormat().equals(newDatastream.getObservationType().getFormat()))
+                    map.add("observationType");
+                if (!oldDatastream.getUnitOfMeasurement().equals(newDatastream.getUnitOfMeasurement()))
+                    map.add("unitOfMeasurement");
+                if (!oldDatastream.getGeometryEntity().getGeometry().equals(newDatastream.getGeometryEntity().getGeometry()))
+                    map.add("observedArea");
+                if (!oldDatastream.getSamplingTimeStart().equals(newDatastream.getSamplingTimeStart()))
+                    map.add("phenomenonTime");
+                if (!oldDatastream.getSamplingTimeEnd().equals(newDatastream.getSamplingTimeEnd()))
+                    map.add("phenomenonTime");
+                if (!oldDatastream.getResultTimeStart().equals(newDatastream.getResultTimeStart()))
+                    map.add("resultTime");
+                if (!oldDatastream.getResultTimeEnd().equals(newDatastream.getResultTimeEnd())) map.add("resultTime");
+                return map;
             case "HistoricalLocationEntity":
+                HistoricalLocationEntity oldHLocation = (HistoricalLocationEntity) oldE;
+                HistoricalLocationEntity newHLocation = (HistoricalLocationEntity) newE;
+                if (!oldHLocation.getTime().equals(newHLocation.getTime())) map.add("time");
+                return map;
             case "DataEntity":
-                //TODO: check if this abstract type is enough or concrete type is needed
+                DataEntity<?> oldData = (DataEntity<?>) oldE;
+                DataEntity<?> newData = (DataEntity<?>) newE;
+                if (!oldData.getSamplingTimeStart().equals(newData.getSamplingTimeStart())) map.add("phenomenonTime");
+                if (!oldData.getSamplingTimeEnd().equals(newData.getSamplingTimeEnd())) map.add("phenomenonTime");
+                if (!oldData.getResultTime().equals(newData.getResultTime())) map.add("resultTime");
+                if (!oldData.getValidTimeStart().equals(newData.getValidTimeStart())) map.add("validTime");
+                if (!oldData.getValidTimeEnd().equals(newData.getValidTimeEnd())) map.add("validTime");
+                //TODO: implement difference map for ::getParameters and ::getResult and "resultQuality"
+                return map;
             case "FeatureEntity":
+                FeatureEntity oldFeature = (FeatureEntity) oldE;
+                FeatureEntity newFeature = (FeatureEntity) newE;
+                if (!oldFeature.getName().equals(newFeature.getName())) map.add("name");
+                if (!oldFeature.getDescription().equals(newFeature.getDescription())) map.add("description");
+                if (!oldFeature.getGeometry().equals(newFeature.getGeometry())) map.add("feature");
+                // There is only a single allowed encoding type so it cannot change
+                return map;
             case "PhenomenonEntity":
+                PhenomenonEntity oldPhenom = (PhenomenonEntity) oldE;
+                PhenomenonEntity newPhenom = (PhenomenonEntity) newE;
+                if (!oldPhenom.getName().equals(newPhenom.getName())) map.add("name");
+                if (!oldPhenom.getDescription().equals(newPhenom.getDescription())) map.add("description");
+                if (!oldPhenom.getIdentifier().equals(newPhenom.getIdentifier())) map.add("definition");
+                return map;
             default:
                 return map;
         }
