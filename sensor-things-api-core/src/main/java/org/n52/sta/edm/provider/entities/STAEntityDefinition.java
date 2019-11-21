0cd2e70375f9232e73689369607f7cc6cb2845c8
speckij <speckij@gmail.com>
2019-11-21 15:09:01 +0100

    refactor constants to be final

--- sensor-things-api-core/src/main/java/org/n52/sta/edm/provider/entities/STAEntityDefinition.java
+++ sensor-things-api-core/src/main/java/org/n52/sta/edm/provider/entities/STAEntityDefinition.java
@@ -2,16 +2,16 @@ package org.n52.sta.edm.provider.entities;
 
 public abstract class STAEntityDefinition {
 
-    public static String DATASTREAMS = "Datastreams";
-    public static String OBSERVATIONS = "Observations";
-    public static String THINGS = "Things";
-    public static String LOCATIONS = "Locations";
-    public static String HISTORICAL_LOCATIONS = "HistoricalLocations";
-    public static String SENSORS = "Sensors";
-    public static String OBSERVED_PROPERTIES = "ObservedProperties";
-    public static String FEATURES_OF_INTEREST = "FeaturesOfInterest";
+    public static final String DATASTREAMS = "Datastreams";
+    public static final String OBSERVATIONS = "Observations";
+    public static final String THINGS = "Things";
+    public static final String LOCATIONS = "Locations";
+    public static final String HISTORICAL_LOCATIONS = "HistoricalLocations";
+    public static final String SENSORS = "Sensors";
+    public static final String OBSERVED_PROPERTIES = "ObservedProperties";
+    public static final String FEATURES_OF_INTEREST = "FeaturesOfInterest";
 
-    public static String[] allCollections = new String[] {
+    public static final String[] allCollections = new String[] {
             DATASTREAMS,
             OBSERVATIONS,
             THINGS,
@@ -22,35 +22,35 @@ public abstract class STAEntityDefinition {
             FEATURES_OF_INTEREST
     };
 
-    public static String DATASTREAM = "Datastream";
-    public static String OBSERVATION = "Observation";
-    public static String THING = "Thing";
-    public static String LOCATION = "Location";
-    public static String HISTORICAL_LOCATION = "HistoricalLocation";
-    public static String SENSOR = "Sensor";
-    public static String OBSERVED_PROPERTY = "ObservedProperty";
-    public static String FEATURE_OF_INTEREST = "FeatureofInterest";
+    public static final String DATASTREAM = "Datastream";
+    public static final String OBSERVATION = "Observation";
+    public static final String THING = "Thing";
+    public static final String LOCATION = "Location";
+    public static final String HISTORICAL_LOCATION = "HistoricalLocation";
+    public static final String SENSOR = "Sensor";
+    public static final String OBSERVED_PROPERTY = "ObservedProperty";
+    public static final String FEATURE_OF_INTEREST = "FeatureofInterest";
 
     // Entity Property Names
-    public static String PROP_ID = "id";
-    public static String PROP_SELF_LINK = "selfLink";
-    public static String PROP_DEFINITION = "definition";
-    public static String PROP_DESCRIPTION = "description";
-    public static String PROP_ENCODINGTYPE = "encodingType";
-    public static String PROP_FEATURE = "feature";
-    public static String PROP_LOCATION = "location";
-    public static String PROP_NAME = "name";
-    public static String PROP_OBSERVATION_TYPE = "observationType";
-    public static String PROP_OBSERVED_AREA = "observedArea";
-    public static String PROP_PARAMETERS = "parameters";
-    public static String PROP_PHENOMENON_TIME = "phenomenonTime";
-    public static String PROP_PROPERTIES = "properties";
-    public static String PROP_RESULT = "result";
-    public static String PROP_RESULT_QUALITY = "resultQuality";
-    public static String PROP_RESULT_TIME = "resultTime";
-    public static String PROP_TIME = "time";
-    public static String PROP_UOM = "unitOfMeasurement";
-    public static String PROP_VALID_TIME = "validTime";
-    public static String PROP_METADATA = "metadata";
-    public static String PROP_SYMBOL = "symbol";
+    public static final String PROP_ID = "id";
+    public static final String PROP_SELF_LINK = "selfLink";
+    public static final String PROP_DEFINITION = "definition";
+    public static final String PROP_DESCRIPTION = "description";
+    public static final String PROP_ENCODINGTYPE = "encodingType";
+    public static final String PROP_FEATURE = "feature";
+    public static final String PROP_LOCATION = "location";
+    public static final String PROP_NAME = "name";
+    public static final String PROP_OBSERVATION_TYPE = "observationType";
+    public static final String PROP_OBSERVED_AREA = "observedArea";
+    public static final String PROP_PARAMETERS = "parameters";
+    public static final String PROP_PHENOMENON_TIME = "phenomenonTime";
+    public static final String PROP_PROPERTIES = "properties";
+    public static final String PROP_RESULT = "result";
+    public static final String PROP_RESULT_QUALITY = "resultQuality";
+    public static final String PROP_RESULT_TIME = "resultTime";
+    public static final String PROP_TIME = "time";
+    public static final String PROP_UOM = "unitOfMeasurement";
+    public static final String PROP_VALID_TIME = "validTime";
+    public static final String PROP_METADATA = "metadata";
+    public static final String PROP_SYMBOL = "symbol";
 }
