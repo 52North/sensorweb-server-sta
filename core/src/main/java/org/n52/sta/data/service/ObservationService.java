740468e85613759887f2c0d8e2bb9839aa914594
speckij <speckij@gmail.com>
2020-09-28 14:28:26 +0200

    fix Observation->Parameters not being persisted
    core/src/main/java/org/n52/sta/data/service/ObservationService.java

--- core/src/main/java/org/n52/sta/data/service/ObservationService.java
+++ core/src/main/java/org/n52/sta/data/service/ObservationService.java
@@ -34,6 +34,7 @@ import org.n52.series.db.beans.AbstractDatasetEntity;
 import org.n52.series.db.beans.AbstractFeatureEntity;
 import org.n52.series.db.beans.DataEntity;
 import org.n52.series.db.beans.DatasetAggregationEntity;
+import org.n52.series.db.beans.parameter.observation.ObservationParameterEntity;
 import org.n52.series.db.beans.sta.AbstractObservationEntity;
 import org.n52.series.db.beans.sta.BooleanObservationEntity;
 import org.n52.series.db.beans.sta.CategoryObservationEntity;
@@ -77,6 +78,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * @author <a href="mailto:j.speckamp@52north.org">Jan Speckamp</a>
@@ -310,6 +312,22 @@ public class ObservationService
                 // Save Observation
                 ObservationEntity<?> data = saveObservation(observation, observation.getDataset());
 
+                // Save parameters
+                if (observation.getParameters() != null) {
+                    parameterRepository.saveAll(
+                        observation
+                            .getParameters()
+                            .stream()
+                            .filter(o -> o instanceof ObservationParameterEntity)
+                            .map(o -> {
+                                ((ObservationParameterEntity<?>) o).setObservation(data);
+                                return (ObservationParameterEntity) o;
+                            })
+                            .collect(Collectors.toSet())
+                    );
+                    data.setParameters(observation.getParameters());
+                }
+
                 // Update FirstValue/LastValue + FirstObservation/LastObservation of Dataset + Aggregation
                 updateDataset(observation.getDataset(), data);
                 return data;
@@ -377,16 +395,25 @@ public class ObservationService
             existing.setValidTimeEnd(toMerge.getValidTimeEnd());
         }
         // parameter
-        /*
         if (toMerge.getParameters() != null) {
             synchronized (getLock(String.valueOf(
                 toMerge.getParameters().hashCode() + existing.getParameters().hashCode()))) {
-                parameterRepository.saveAll(toMerge.getParameters());
-                existing.getParameters().forEach(parameterRepository::delete);
+                parameterRepository.saveAll(toMerge.getParameters()
+                                                .stream()
+                                                .filter(o -> o instanceof ObservationParameterEntity)
+                                                .map(o -> {
+                                                    ((ObservationParameterEntity<?>) o).setObservation(existing);
+                                                    return (ObservationParameterEntity) o;
+                                                })
+                                                .collect(Collectors.toSet()));
+                existing.getParameters()
+                    .stream()
+                    .filter(o -> o instanceof ObservationParameterEntity)
+                    .map(o -> (ObservationParameterEntity) o)
+                    .forEach(parameterRepository::delete);
                 existing.setParameters(toMerge.getParameters());
             }
         }
-        */
         // value
         if (toMerge.getValue() != null) {
             checkValue(existing, toMerge);
@@ -710,13 +737,6 @@ public class ObservationService
         data.setSamplingGeometry(observation.getSamplingGeometry());
         data.setVerticalFrom(observation.getVerticalFrom());
         data.setVerticalTo(observation.getVerticalTo());
-
-        /*
-        if (observation.getParameters() != null) {
-            parameterRepository.saveAll(observation.getParameters());
-            data.setParameters(observation.getParameters());
-        }
-         */
         return data;
     }
 }
