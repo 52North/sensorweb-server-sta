8ceddea03c775ff7988069ed6a2237a5508d64de
Henning Bredel <bredel@52north.org>
2022-04-27 21:19:18 +0200

    Prepare data-provider tests

--- api/src/main/java/org/n52/sta/api/service/EntityService.java
+++ api/src/main/java/org/n52/sta/api/service/EntityService.java
@@ -4,5 +4,5 @@ import org.n52.sta.api.EntityProvider;
 import org.n52.sta.api.entity.Identifiable;
 
 public interface EntityService<T extends Identifiable> extends EntityProvider<T> {
-    
+
 }
---
+++ data-provider-hibernate/src/test/java/org/n52/sta/data/provider/ThingEntityProviderTest.java
@@ -0,0 +1,42 @@
+package org.n52.sta.data.provider;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.n52.sta.api.EntityPage;
+import org.n52.sta.api.entity.Thing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class ThingEntityProviderTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThingEntityProviderTest.class);
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private ThingEntityProvider provider;
+
+    @Test
+    public void test1() {
+        assertThat(context, is(not(Matchers.nullValue())));
+    }
+
+    @Test
+    public void test() {
+        LOGGER.debug("Testing starts");
+        EntityPage<Thing> emptyPage = provider.getEntities();
+        assertThat(emptyPage.getTotalCount(), is(0));
+    }
+
+}
