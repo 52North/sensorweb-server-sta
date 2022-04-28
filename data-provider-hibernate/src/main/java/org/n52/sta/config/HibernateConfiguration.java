b2f820c9f0bc4b021b6b75efd61122daa6e3104e
Henning Bredel <bredel@52north.org>
2022-04-28 13:13:57 +0200

    Add separated HibernateConfiguration

---
+++ data-provider-hibernate/src/main/java/org/n52/sta/config/HibernateConfiguration.java
@@ -0,0 +1,52 @@
+package org.n52.sta.config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.hibernate.boot.model.TypeContributor;
+import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl;
+import org.hibernate.jpa.boot.spi.TypeContributorList;
+import org.hibernate.type.BasicType;
+import org.n52.hibernate.type.SmallBooleanType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class HibernateConfiguration {
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory(
+            @Value("${database.jpa.persistence-location}") String persistenceXmlLocation,
+            DataSource datasource, JpaProperties properties) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        emf.setJpaPropertyMap(addCustomTypes(properties));
+        emf.setPersistenceXmlLocation(persistenceXmlLocation);
+        emf.setDataSource(datasource);
+        emf.afterPropertiesSet();
+        return emf.getNativeEntityManagerFactory();
+    }
+
+    private Map<String, Object> addCustomTypes(JpaProperties jpaProperties) {
+        Map<String, Object> properties = new HashMap<>(jpaProperties.getProperties());
+        properties.put(EntityManagerFactoryBuilderImpl.TYPE_CONTRIBUTORS, createTypeContributorsList());
+        return properties;
+    }
+
+    private TypeContributorList createTypeContributorsList() {
+        return () -> Arrays.asList(toTypeContributor(SmallBooleanType.INSTANCE, "small_boolean"));
+    }
+
+    private <T extends BasicType> TypeContributor toTypeContributor(T type, String... keys) {
+        return (typeContributions, serviceRegistry) -> typeContributions.contributeType(type, keys);
+    }
+
+}
