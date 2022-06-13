4b5926d91a09e03ccf032da661b20c681ebadbe1
Henning Bredel <bredel@52north.org>
2022-06-13 14:26:42 +0200

    Refactor deserializer to make instance factory re-usable

--- http/src/main/java/org/n52/sta/http/serialize/in/StaEntityDeserializer.java
+++ http/src/main/java/org/n52/sta/http/serialize/in/StaEntityDeserializer.java
@@ -4,7 +4,14 @@ package org.n52.sta.http.serialize.in;
 import java.io.IOException;
 import java.util.function.BiFunction;
 
+import org.n52.sta.api.entity.Datastream;
+import org.n52.sta.api.entity.FeatureOfInterest;
+import org.n52.sta.api.entity.HistoricalLocation;
 import org.n52.sta.api.entity.Identifiable;
+import org.n52.sta.api.entity.Location;
+import org.n52.sta.api.entity.Observation;
+import org.n52.sta.api.entity.ObservedProperty;
+import org.n52.sta.api.entity.Sensor;
 import org.n52.sta.api.entity.Thing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,29 +29,44 @@ public class StaEntityDeserializer<T extends Identifiable> extends StdDeserializ
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StaEntityDeserializer.class);
 
+    private final BiFunction<JsonNode, ObjectMapper, ? extends Identifiable> instanceFactory;
+    
     private final ObjectMapper mapper;
 
     public StaEntityDeserializer(Class<T> entityType, ObjectMapper mapper) {
         super(entityType);
+        this.instanceFactory = createInstanceFactory();
         this.mapper = mapper;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-
         JsonNode node = ctxt.readTree(p);
-        if (Thing.class.isAssignableFrom(handledType())) {
-            return wrapNode(node, ThingNode::new);
-        } else {
-            LOGGER.debug("Invalid input for type '{}': {}", handledType(), node.toPrettyString());
-            throw new IllegalStateException("Unknown type to serialize: " + handledType());
-        }
-
+        return (T) instanceFactory.apply(node, mapper);
     }
 
-    @SuppressWarnings("unchecked")
-    private T wrapNode(JsonNode node, BiFunction<JsonNode, ObjectMapper, ? extends Identifiable> create) {
-        return (T) create.apply(node, mapper);
+    private BiFunction<JsonNode, ObjectMapper, ? extends Identifiable> createInstanceFactory() {
+        if (Thing.class.isAssignableFrom(handledType())) {
+            return ThingNode::new;
+        } else if (Datastream.class.isAssignableFrom(handledType())) {
+            return DatastreamNode::new;
+        } else if (Sensor.class.isAssignableFrom(handledType())) {
+            return SensorNode::new;
+        } else if (Location.class.isAssignableFrom(handledType())) {
+            return LocationNode::new;
+        } else if (HistoricalLocation.class.isAssignableFrom(handledType())) {
+            return HistoricalLocationNode::new;
+        } else if (ObservedProperty.class.isAssignableFrom(handledType())) {
+            return ObservedPropertyNode::new;
+        } else if (Observation.class.isAssignableFrom(handledType())) {
+            return ObservationNode::new;
+        } else if (FeatureOfInterest.class.isAssignableFrom(handledType())) {
+            return FeatureOfInterestNode::new;
+        } else {
+            LOGGER.debug("Unknown input type '{}'", handledType());
+            throw new IllegalStateException("Unknown type to serialize: " + handledType());
+        }
     }
 
 }
