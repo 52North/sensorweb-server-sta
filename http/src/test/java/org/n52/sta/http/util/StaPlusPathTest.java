6bdc987786f92da07ef1e9ca3f8e3d05844699d6
CarstenHollmann <c.hollmann@52north.org>
2022-12-21 15:57:45 +0100

    add furhter tests

--- http/src/test/java/org/n52/sta/http/util/StaPlusPathTest.java
+++ http/src/test/java/org/n52/sta/http/util/StaPlusPathTest.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (C) 2018-2022 52°North Spatial Information Research GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
 package org.n52.sta.http.util;
 
 import java.util.List;
@@ -57,27 +84,80 @@ public class StaPlusPathTest {
 
     @Test
     public void assert_relation_has_member_subject() throws STAInvalidUrlException {
-        
+
         // TODO why these examples do not fail?
         //StaPath<? extends Identifiable> path = factory.parse("/Relations(sdf)/Observations");
         //StaPath<? extends Identifiable> path = factory.parse("/Things(sdf)/Relations(sdf);
-        
+
         StaPath<? extends Identifiable> path = factory.parse("/Relations(sdf)/Subject");
         MatcherAssert.assertThat(path.getPathType(), is(SelectPath.PathType.entity));
         List<PathSegment> segments = path.getPathSegments();
         MatcherAssert.assertThat(segments.size(), is(2));
 
-        PathSegment relations = segments.get(0);
-        MatcherAssert.assertThat(relations.getCollection(), is("Subjects"));
-        MatcherAssert.assertThat(relations.getIdentifier(), is(Optional.empty()));
-        MatcherAssert.assertThat(relations.getProperty(), is(Optional.empty()));
+        PathSegment subject = segments.get(0);
+        MatcherAssert.assertThat(subject.getCollection(), is("Subject"));
+        MatcherAssert.assertThat(subject.getIdentifier(), is(Optional.empty()));
+        MatcherAssert.assertThat(subject.getProperty(), is(Optional.empty()));
 
-        PathSegment group = segments.get(1);
-        MatcherAssert.assertThat(group.getCollection(), is("Relations"));
-        MatcherAssert.assertThat(group.getIdentifier(), is(Optional.of("sdf")));
-        MatcherAssert.assertThat(group.getProperty(), is(Optional.empty()));
+        PathSegment relations = segments.get(1);
+        MatcherAssert.assertThat(relations.getCollection(), is("Relations"));
+        MatcherAssert.assertThat(relations.getIdentifier(), is(Optional.of("sdf")));
+        MatcherAssert.assertThat(relations.getProperty(), is(Optional.empty()));
     }
 
+    @Test
+    public void assert_relation_has_member_object() throws STAInvalidUrlException {
+        StaPath<? extends Identifiable> path = factory.parse("/Relations(sdf)/Object");
+        MatcherAssert.assertThat(path.getPathType(), is(SelectPath.PathType.entity));
+        List<PathSegment> segments = path.getPathSegments();
+        MatcherAssert.assertThat(segments.size(), is(2));
+
+        PathSegment object = segments.get(0);
+        MatcherAssert.assertThat(object.getCollection(), is("Object"));
+        MatcherAssert.assertThat(object.getIdentifier(), is(Optional.empty()));
+        MatcherAssert.assertThat(object.getProperty(), is(Optional.empty()));
+
+        PathSegment relations = segments.get(1);
+        MatcherAssert.assertThat(relations.getCollection(), is("Relations"));
+        MatcherAssert.assertThat(relations.getIdentifier(), is(Optional.of("sdf")));
+        MatcherAssert.assertThat(relations.getProperty(), is(Optional.empty()));
+    }
+
+    @Test
+    public void assert_observation_has_member_subjects() throws STAInvalidUrlException {
+        StaPath<? extends Identifiable> path = factory.parse("/Observations(sdf)/Subjects");
+        MatcherAssert.assertThat(path.getPathType(), is(SelectPath.PathType.collection));
+        List<PathSegment> segments = path.getPathSegments();
+        MatcherAssert.assertThat(segments.size(), is(2));
+
+        PathSegment subjects = segments.get(0);
+        MatcherAssert.assertThat(subjects.getCollection(), is("Subjects"));
+        MatcherAssert.assertThat(subjects.getIdentifier(), is(Optional.empty()));
+        MatcherAssert.assertThat(subjects.getProperty(), is(Optional.empty()));
+
+        PathSegment observations = segments.get(1);
+        MatcherAssert.assertThat(observations.getCollection(), is("Observations"));
+        MatcherAssert.assertThat(observations.getIdentifier(), is(Optional.of("sdf")));
+        MatcherAssert.assertThat(observations.getProperty(), is(Optional.empty()));
+    }
+
+    @Test
+    public void assert_observation_has_member_objects() throws STAInvalidUrlException {
+        StaPath<? extends Identifiable> path = factory.parse("/Observations(sdf)/Objects");
+        MatcherAssert.assertThat(path.getPathType(), is(SelectPath.PathType.collection));
+        List<PathSegment> segments = path.getPathSegments();
+        MatcherAssert.assertThat(segments.size(), is(2));
+
+        PathSegment objects = segments.get(0);
+        MatcherAssert.assertThat(objects.getCollection(), is("Objects"));
+        MatcherAssert.assertThat(objects.getIdentifier(), is(Optional.empty()));
+        MatcherAssert.assertThat(objects.getProperty(), is(Optional.empty()));
+
+        PathSegment observations = segments.get(1);
+        MatcherAssert.assertThat(observations.getCollection(), is("Observations"));
+        MatcherAssert.assertThat(observations.getIdentifier(), is(Optional.of("sdf")));
+        MatcherAssert.assertThat(observations.getProperty(), is(Optional.empty()));
+    }
 
     @Test
     public void assert_relation_has_property_role() throws STAInvalidUrlException {
